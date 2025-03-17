4a8390bc8e84dd52027b45c037fd4d4187c7b8f8
nybia <adalel@my.bcit.ca>
2025-03-17 13:19:07 -0700

    Delete example code

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.comp2522202510termprojectaibyn {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.comp2522202510termprojectaibyn to javafx.fxml;
-    exports com.example.comp2522202510termprojectaibyn;
-}
